29ef5c7dab80991ba673c9036af944f8c82af6bb
CarlMenger <56433847+CarlMenger@users.noreply.github.com>
2019-12-24 14:55:29 +0100

    Update [I]Problems.py

--- [I]Problems.py
+++ [I]Problems.py
@@ -1,6 +1,24 @@
 """ My solutions from daily newslatter of TechLead
 """
 
-# **2019** 
 
-# 24.12 - Algorithm, Find any combination of two numbers that add up to k.
+import random
+import numpy as np
+
+
+def sum_sorting_list(input_list, k):
+    """ 24.12.2019. Find any combination of 2 number_index from list, that summ up to k """
+    output_list = []
+    print(input_list)
+    for number_index in range(len(input_list)):
+        for i in range(number_index, len(input_list)):
+            if input_list[number_index] + input_list[i] == k:
+                output_list.append((input_list[number_index], input_list[i]))
+    return output_list
+
+input_list = np.random.randint(1, 20, size=100)
+print(sum_sorting_list(input_list, 20))
+
+#**********************************************************************************************************************************
+
+
