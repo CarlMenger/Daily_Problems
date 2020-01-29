ffe857ba9c49c2e584fadf3d0dea49f5f14b8bc2
CarlMenger <56433847+CarlMenger@users.noreply.github.com>
2020-01-29 16:52:01 +0100

    Create [24.12.2019] Combination of any 2 number_index from list, that summ up to k.py

---
+++ [24.12.2019] Combination of any 2 number_index from list, that summ up to k.py
@@ -0,0 +1,17 @@
+import random
+import numpy as np
+import math
+
+
+def sum_sorting_list(input_list, k):
+    """ 24.12.2019. Find any combination of 2 number_index from list, that summ up to k """
+    output_list = []
+    print(input_list)
+    for number_index in range(len(input_list)):
+        for i in range(number_index, len(input_list)):
+            if input_list[number_index] + input_list[i] == k:
+                output_list.append((input_list[number_index], input_list[i]))
+    return output_list
+
+input_list = np.random.randint(1, 20, size=100)
+print(sum_sorting_list(input_list, 20))
