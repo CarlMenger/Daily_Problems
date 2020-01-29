d7ecc073fec0dad69495cfc3c7c347d80ecf3f01
CarlMenger <56433847+CarlMenger@users.noreply.github.com>
2020-01-29 16:53:07 +0100

    Create [25.12.2019] Given an array of integers, return a new array such that each element at index i of the new array  is the product of  all the numbers in the original array except the one at i.py

---
+++ [25.12.2019] Given an array of integers, return a new array such that each element at index i of the new array  is the product of  all the numbers in the original array except the one at i.py
@@ -0,0 +1,14 @@
+
+""" 25.12.2019. Given an array of integers, return a new array such that each element at index i of the new array 
+is the product of  all the numbers in the original array except the one at i. For example,
+ if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], 
+ the expected output would be [2, 3, 6]."""
+
+
+def d19_12_25_b(input_list):
+    clear_lists = [[value for index, value in enumerate(input_list) if index != i] for i in range(len(input_list))]
+    return [np.prod(x) for x in clear_lists]
+
+
+input_list = [1, 2, 3, 4, 5]
+print(d19_12_25_b(input_list))
